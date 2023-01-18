d0e7a1c53764126f89ce264d9d49759f6fe327a4
Hidenori Shinohara <hidenori@stellar.org>
2023-01-18 14:51:50 -0500

    update

--- player/src/lib.rs
+++ player/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
+use soroban_sdk::{contractimpl, symbol, vec, Env, Symbol, Vec};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Contract;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[contractimpl]
+impl Contract {
+    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
+        vec![&env, symbol!("Hello"), to]
     }
 }
